8c81df9470cee23d498fe4770accff695ca71843
hyestar <0109jhs@naver.com>
2022-07-16 13:50:53 +0900

    게시물 상세페이지 기능 구현, 날짜는 처리 못함

--- src/com/it/java/ssg/Main.java
+++ src/com/it/java/ssg/Main.java
@@ -43,12 +43,36 @@ public class Main {
 //				}
 //				최신게시물부터 출력
 				System.out.println("번호  |    제목");
-				for(int i = articles.size()-1; i>=0;i--) {
+				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 					System.out.printf("%d    |  %s\n", article.id, article.title);
 				}
+
+			} else if (command.startsWith("article detail")) {
+				String[] commandbits = command.split(" ");
+				int id = Integer.parseInt(commandbits[2]); // "2" => 2
 				
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						
+						foundArticle = article;
+						break;
+					}
+				}
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : 2022-07-16 12:41:15\n");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
 			} else {
+
 				System.out.printf("%s는 존재하지 않는 명령어 입니다.\n", command);
 			}
 		}
