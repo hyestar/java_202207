c121017f69a344e18c9e100866c0ed66cb7f9ecb
hyestar <0109jhs@naver.com>
2022-07-17 12:58:04 +0900

    게시물 저장시 작성날짜 저장

--- src/com/it/java/ssg/Main.java
+++ src/com/it/java/ssg/Main.java
@@ -25,11 +25,12 @@ public class Main {
 			if (command.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
+				String regDate = Util.main();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regDate, title, body);
 				articles.add(article);
 				System.out.printf("%d번째 글이 생성되었습니다.\n", id);
 			} else if (command.equals("article list")) {
@@ -67,7 +68,7 @@ public class Main {
 					continue;
 				}
 				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : 2022-07-16 12:41:15\n");
+				System.out.printf("날짜 : %s\n", foundArticle.regDate);
 				System.out.printf("제목 : %s\n", foundArticle.title);
 				System.out.printf("내용 : %s\n", foundArticle.body);
 
@@ -107,11 +108,13 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
-	public Article(int id, String title, String body) {
+	public Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
 	}
